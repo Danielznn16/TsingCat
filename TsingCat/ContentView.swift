82b924510bc0b6ac08fccc42a0ac3d5b39fc83b4
Daniel Zhang-Li <1098811384@qq.com>
2020-06-27 18:02:03 +0800

    boiler

---
+++ TsingCat/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TsingCat
+//
+//  Created by Daniel Zhang-Li on 2020/6/27.
+//  Copyright © 2020 Daniel Zhang-Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
